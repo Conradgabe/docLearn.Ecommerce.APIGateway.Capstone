30f040c40949e9637a80a3c5729a162f264809f8
Conradgabe <gisuekebho5880@gmail.com>
2025-06-04 10:15:57 +0100

    Add project files.

---
+++ APIGateway.Presentation/MIddleware/AttachedSignatureRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace APIGateway.Presentation.MIddleware
+{
+    public class AttachedSignatureRequest(RequestDelegate next)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            context.Request.Headers["API-Gateway"] = "Signed";
+            await next(context);
+        }
+    }
+}
